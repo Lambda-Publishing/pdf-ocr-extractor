7d1a61c6560b650d1ef45881437d9ded3928a50d
behike56 <engineering@lambda-publishing.dev>
2025-06-07 13:20:20 +0900

    Chane code for pdfbox version 3.

--- src/app/core/service/pdf.clj
+++ src/app/core/service/pdf.clj
@@ -1,10 +1,18 @@
 (ns app.core.service.pdf
   (:require [clojure.java.io :as io])
-  (:import  [org.apache.pdfbox.pdmodel     PDDocument]
-            [org.apache.pdfbox.rendering   PDFRenderer ImageType]))
+  (:import (org.apache.pdfbox Loader)                         ;; ← NEW
+           (org.apache.pdfbox.rendering PDFRenderer ImageType)
+           (org.apache.pdfbox.io IOUtils)))
 
-(defn pages->images [pdf-path]
-  (with-open [doc (PDDocument/load (io/file pdf-path))]
+
+(defn pages->images
+  "Return a vector of BufferedImages (300 DPI, grayscale) for every
+   page in the PDF at `pdf-path`.  Uses a *temp-file-only* scratch cache,
+   which keeps memory usage low for large PDFs."
+  [pdf-path]
+  (with-open [doc (Loader/loadPDF                                   ;; ← NEW
+                   (io/file pdf-path)
+                   (IOUtils/createTempFileOnlyStreamCache))]       ;; ← NEW
     (let [renderer (PDFRenderer. doc)]
-      (mapv #(renderer/renderImageWithDPI % 300 ImageType/GRAY)
+      (mapv #(.renderImageWithDPI renderer % 300 ImageType/GRAY)
             (range (.getNumberOfPages doc))))))
